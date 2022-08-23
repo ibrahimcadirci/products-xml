42a4aba6cf6a27ac1b21bde27dc8e8a3dfe7b29c
İbrahim Halil Çadırcı <36551642+ibrahimcadirci@users.noreply.github.com>
2022-08-23 14:57:46 +0300

    adds flex xml instance

--- index.php
+++ index.php
@@ -12,4 +12,11 @@ if (isset($_GET['rss']) && $_GET['rss'] == 1) {
     $xmlFeed->setDescription("Karaca Home Description");
     $xmlFeed->setLang("tr");
     echo $xmlFeed->run();
-}
+}else if (isset($_GET['rss']) && $_GET['rss'] == 2) {
+     $xmlFeed->setTitle("Karaca Home");
+     $xmlFeed->setUrl("www.karaca.com");
+     $xmlFeed->setDescription("Karaca Home Description");
+     $xmlFeed->setLang("tr");
+     echo $xmlFeed->run(["ID", "TITLE", "PRICE", "CATEGORY"]);
+ }
+
