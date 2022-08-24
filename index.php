d1f0d6c1099af51bda40dd11020e5fc310ae1177
İbrahim Halil Çadırcı <36551642+ibrahimcadirci@users.noreply.github.com>
2022-08-24 13:01:34 +0300

    Update index.php

--- index.php
+++ index.php
@@ -7,15 +7,15 @@ header('Content-type: text/xml');
 $xmlFeed = new xmlFeed();
 
 if (isset($_GET['rss']) && $_GET['rss'] == 1) {
-    $xmlFeed->setTitle("Karaca Home");
-    $xmlFeed->setUrl("www.karaca.com");
-    $xmlFeed->setDescription("Karaca Home Description");
+    $xmlFeed->setTitle("Example");
+    $xmlFeed->setUrl("www.example.com");
+    $xmlFeed->setDescription("Example Description");
     $xmlFeed->setLang("tr");
     echo $xmlFeed->run();
 }else if (isset($_GET['rss']) && $_GET['rss'] == 2) {
-     $xmlFeed->setTitle("Karaca Home");
-     $xmlFeed->setUrl("www.karaca.com");
-     $xmlFeed->setDescription("Karaca Home Description");
+     $xmlFeed->setTitle("Example Home");
+     $xmlFeed->setUrl("www.example.com");
+     $xmlFeed->setDescription("Example Description");
      $xmlFeed->setLang("tr");
      echo $xmlFeed->run(["ID", "TITLE", "PRICE", "CATEGORY"]);
  }
